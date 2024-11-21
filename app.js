5761792c26a786995bb6bc499fb1d74b5742fa94
Zvi Haffner <zvihaffner@gmail.com>
2024-11-21 16:32:48 +0000

    Adds middleware to parse JSON requests. Handles errors for not-null and foreign key constraints.

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const apiRouter = require("./routers/api-router");
 const app = express();
 
+app.use(express.json());
+
 app.use("/api", apiRouter);
 
 app.use((err, req, res, next) => {
@@ -11,8 +13,14 @@ app.use((err, req, res, next) => {
 });
 
 app.use((err, req, res, next) => {
-  if (err.code === "22P02" || err.code === "22007") {
+  if (err.code === "22P02" || err.code === "22007" || err.code === "23502") {
     res.status(400).send({ msg: "Bad Request" });
+  } else next(err);
+});
+
+app.use((err, req, res, next) => {
+  if (err.code === "23503") {
+    res.status(404).send({ msg: "Not Found" });
   } else next(err);
 });
 
