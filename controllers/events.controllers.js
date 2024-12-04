95e05d2b8da10db17edf923efdacab5dedb85237
Zvi Haffner <zvihaffner@gmail.com>
2024-12-04 14:54:11 +0000

    Creates and tests endpoint to patch an event

--- controllers/events.controllers.js
+++ controllers/events.controllers.js
@@ -1,10 +1,20 @@
-const { insertEvent } = require("../models/events.models");
+const { insertEvent, updateEventById } = require("../models/events.models");
 
 exports.addEvent = (req, res, next) => {
   const newEvent = req.body;
   insertEvent(newEvent)
     .then((event) => {
       res.status(201).send({ event });
+    })
+    .catch(next);
+};
+
+exports.patchEventById = (req, res, next) => {
+  const { id } = req.params;
+  const { column, value } = req.body;
+  updateEventById(id, column, value)
+    .then((updatedEvent) => {
+      res.status(200).send({ updatedEvent });
     })
     .catch(next);
 };
